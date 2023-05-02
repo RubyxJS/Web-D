b4825f229c50c006c325c168d152e384f8610d4b
Reuben <68828483+RubyxJS@users.noreply.github.com>
2023-05-02 12:27:26 +0100

    wip carousel

--- code/logic.js
+++ code/logic.js
@@ -1,3 +1,7 @@
+/* This is the main function for the about page.
+ * It listens for click events on the buttons in the about content menu and displays the corresponding content on the page.
+ * It achieves this by toggling the "active" class on both the button and the content element. */
+
 (function (){
     const historyButton = document.getElementById("History");
     const cultureButton = document.getElementById("Culture");
@@ -9,6 +13,7 @@
     const landmarksContent = document.getElementById("landmarks-content");
     const allContents = [historyContent, cultureContent, landmarksContent];
     
+    // Handle click events on the History button
     historyButton.addEventListener("click", function(){
         allButtons.forEach(function(button){
             button.classList.remove("active");
@@ -21,6 +26,7 @@
         historyContent.classList.add("active");
     });
 
+    // Handle click events on the Culture button
     cultureButton.addEventListener("click", function(){
         allButtons.forEach(function(button){
             button.classList.remove("active");
@@ -33,6 +39,7 @@
         cultureContent.classList.add("active");
     });
 
+    // Handle click events on the Landmarks button
     landmarksButton.addEventListener("click", function(){
         allButtons.forEach(function(button){
             button.classList.remove("active");
@@ -44,4 +51,4 @@
         });
         landmarksContent.classList.add("active");
     });
-})()
+})();
